e1355a0b646b5ac99cc3d41e814218fa7d759230
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-06-21 16:22:15 -0300

    docs: Create list transfer

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('Transferências'),),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    )
-  ));
+      home: Scaffold(
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.00'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.00'),
+            subtitle: Text('1000'),
+          ),
+        ),
+      ],
+    ),
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+    ),
+  )));
 }
