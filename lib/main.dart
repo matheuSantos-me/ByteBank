68a65e73b397c097fd48818f2d49035a88e4e88c
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-06-21 19:16:02 -0300

    docs: Create event for salve date of Bank Transfer

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,10 @@ class BankTransferList extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            CardBankTransfer(ValuesBankTransfer(100.00, '587458-7')),
-            CardBankTransfer(ValuesBankTransfer(200.00, '587458-7')),
-            CardBankTransfer(ValuesBankTransfer(300.00, '587458-7')),
-            CardBankTransfer(ValuesBankTransfer(400.00, '587458-7')),
+            CardBankTransfer(ValuesBankTransfer(100.00, 587458-3)),
+            CardBankTransfer(ValuesBankTransfer(200.00, 587458-7)),
+            CardBankTransfer(ValuesBankTransfer(300.00, 587458-7)),
+            CardBankTransfer(ValuesBankTransfer(400.00, 587458-7)),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -46,7 +46,7 @@ class CardBankTransfer extends StatelessWidget {
       child: ListTile(
         leading: Icon(Icons.monetization_on),
         title: Text(_valuesBankTransfer.valueTransfer.toString()),
-        subtitle: Text(_valuesBankTransfer.bankAccountNumber),
+        subtitle: Text(_valuesBankTransfer.bankAccountNumber.toString()),
       ),
     );
   }
@@ -54,12 +54,22 @@ class CardBankTransfer extends StatelessWidget {
 
 class ValuesBankTransfer {
   final double valueTransfer;
-  final String bankAccountNumber;
+  final int bankAccountNumber;
 
   ValuesBankTransfer(this.valueTransfer, this.bankAccountNumber);
+
+  @override
+  String toString() {
+    return 'ValuesBankTransfer{valor: $valueTransfer, numeroConta $bankAccountNumber}';
+  }
 }
 
 class BankTransferForm extends StatelessWidget {
+  final TextEditingController _controllerBankAccountNumber =
+      TextEditingController();
+  final TextEditingController _controllerBankTransferAmount =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,27 +81,37 @@ class BankTransferForm extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
             child: TextField(
+              controller: _controllerBankAccountNumber,
               style: TextStyle(fontSize: 24.0),
               decoration: InputDecoration(
-                  labelText: 'Número da Conta', hintText: '000000-0'),
+                  labelText: 'Digite o Número da Conta', hintText: '000000-0'),
               keyboardType: TextInputType.number,
             ),
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 32.0),
             child: TextField(
+              controller: _controllerBankTransferAmount,
               style: TextStyle(fontSize: 24.0),
               decoration: InputDecoration(
                   icon: Icon(Icons.monetization_on),
-                  labelText: 'Número da Conta',
-                  hintText: '000000-0'),
+                  labelText: 'Digite o valor a ser Transferido',
+                  hintText: '100,0'),
               keyboardType: TextInputType.number,
             ),
           ),
           RaisedButton(
-            child: Text('CONFIRMAR TRANSFERENCIA'),
+            child: Text('CONFIRMAR TRANSFERÊNCIA'),
             onPressed: () {
-              print('FGHJKLKJHGFDSDFGHJKLKJHGFDSDFGHJK');
+              final double BankTransferAmount =
+                  double.tryParse(_controllerBankTransferAmount.text);
+              final int BankAccountNumber =
+                  int.tryParse(_controllerBankAccountNumber.text);
+              if (BankTransferAmount != null && BankAccountNumber != null) {
+                final CreateBankTransfer =
+                    ValuesBankTransfer(BankTransferAmount, BankAccountNumber);
+                print('$CreateBankTransfer');
+              }
             },
           )
         ],
