302393012010a89c1b0ccb43ad1ae1cb92d0da3e
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-06-21 16:47:36 -0300

    docs: Create class

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    body: Column(
-      children: <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.00'),
-            subtitle: Text('1000'),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.00'),
-            subtitle: Text('1000'),
-          ),
-        ),
-      ],
-    ),
+    body: bankTransferList(),
     appBar: AppBar(
       title: Text('Transferências'),
     ),
@@ -28,4 +11,35 @@ void main() {
       child: Icon(Icons.add),
     ),
   )));
+}
+
+class bankTransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        cardBankTransfer('100.00', '605040-7'),
+        cardBankTransfer('200.00', '605847-8'),
+        cardBankTransfer('303.00', '215487-2'),
+      ],
+    );
+  }
+}
+
+class cardBankTransfer extends StatelessWidget {
+  final String valueTransfer;
+  final String bankAccountNumber;
+
+  cardBankTransfer(this.valueTransfer, this.bankAccountNumber);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(valueTransfer),
+        subtitle: Text(bankAccountNumber),
+      ),
+    );
+  }
 }
