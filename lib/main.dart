90f9cf9d5ba9fac36d7f6af29597e6fb92d9c627
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-06-21 21:25:34 -0300

    docs: class breakdown

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,10 @@ class BankTransferList extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            CardBankTransfer(ValuesBankTransfer(100.00, 587458-3)),
-            CardBankTransfer(ValuesBankTransfer(200.00, 587458-7)),
-            CardBankTransfer(ValuesBankTransfer(300.00, 587458-7)),
-            CardBankTransfer(ValuesBankTransfer(400.00, 587458-7)),
+            CardBankTransfer(ValuesBankTransfer(100.00, 587458 - 3)),
+            CardBankTransfer(ValuesBankTransfer(200.00, 587458 - 7)),
+            CardBankTransfer(ValuesBankTransfer(300.00, 587458 - 7)),
+            CardBankTransfer(ValuesBankTransfer(400.00, 587458 - 7)),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -78,43 +78,58 @@ class BankTransferForm extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
-            child: TextField(
+          Input(
               controller: _controllerBankAccountNumber,
-              style: TextStyle(fontSize: 24.0),
-              decoration: InputDecoration(
-                  labelText: 'Digite o Número da Conta', hintText: '000000-0'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 32.0),
-            child: TextField(
+              placeholder: 'Digite o Número da Conta',
+              label: '000000-0'),
+          Input(
               controller: _controllerBankTransferAmount,
-              style: TextStyle(fontSize: 24.0),
-              decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Digite o valor a ser Transferido',
-                  hintText: '100,0'),
-              keyboardType: TextInputType.number,
+              placeholder: 'Digite o valor a ser Transferido',
+              label: '100.0',
+              iconData: Icons.monetization_on),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
+            child: RaisedButton(
+              child: Text('CONFIRMAR TRANSFERÊNCIA'),
+              onPressed: () {
+                final double BankTransferAmount =
+                    double.tryParse(_controllerBankTransferAmount.text);
+                final int BankAccountNumber =
+                    int.tryParse(_controllerBankAccountNumber.text);
+                if (BankTransferAmount != null && BankAccountNumber != null) {
+                  final CreateBankTransfer =
+                      ValuesBankTransfer(BankTransferAmount, BankAccountNumber);
+                  print('$CreateBankTransfer');
+                }
+              },
             ),
-          ),
-          RaisedButton(
-            child: Text('CONFIRMAR TRANSFERÊNCIA'),
-            onPressed: () {
-              final double BankTransferAmount =
-                  double.tryParse(_controllerBankTransferAmount.text);
-              final int BankAccountNumber =
-                  int.tryParse(_controllerBankAccountNumber.text);
-              if (BankTransferAmount != null && BankAccountNumber != null) {
-                final CreateBankTransfer =
-                    ValuesBankTransfer(BankTransferAmount, BankAccountNumber);
-                print('$CreateBankTransfer');
-              }
-            },
           )
         ],
+      ),
+    );
+  }
+}
+
+class Input extends StatelessWidget {
+  final TextEditingController controller;
+  final String placeholder;
+  final String label;
+  final IconData iconData;
+
+  Input({this.controller, this.placeholder, this.label, this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+            icon: iconData != null ? Icon(iconData) : null,
+            labelText: placeholder,
+            hintText: label),
+        keyboardType: TextInputType.number,
       ),
     );
   }
